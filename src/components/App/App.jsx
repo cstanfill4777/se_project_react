a9dd80d9b714cf29276889396a97eef8a5023522
Christian Stanfill <cstanfill88@gmail.com>
2026-01-27 03:30:32 -0500

    New updates

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,6 +7,7 @@ import Main from "../Main/Main;";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import ItemModal from "../ItemModal/ItemModal";
 import { getWeather } from "../../utils/weatherApi";
+import Footer from "../Footer/Footer";
 
 function App() {
   const [weatherData, setWeatherData] = useState({
@@ -44,6 +45,7 @@ function App() {
         <Header handleAddClick={handleAddClick} weatherData={weatherData} />
         <Main weatherData={weatherData} handleCardClick={handleCardClick} />
       </div>
+      <Footer />
       <ModalWithForm
         title="New garment"
         buttonText="Add garment"
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import "./Footer.css";
+
+function Footer() {
+  return <footer className="footer">{}</footer>;
+}
+
+export { Footer };
+export default Footer;
