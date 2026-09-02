fa3ca58cae79282f7c2d2bf9e4a1665660cd5a3a
Christian Stanfill <cstanfill88@gmail.com>
2026-09-02 01:24:09 -0400

    Configure frontend for production deployment

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-export const baseUrl = "http://localhost:3001";
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.cstanfill4777.servernux.com"
+    : "http://localhost:3001";
 
 export function checkResponse(res) {
   if (!res.ok) {
